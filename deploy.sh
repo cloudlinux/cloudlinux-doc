b84fd0c45115bd0b0eec85e53022d414d3255fd7
Nikolay Telepenin <ntelepenin@cloudlinux.com>
2018-11-12 20:26:28 +0300

    Update fingerprint and domain

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:telepenin/vuepress-template.git master:gh-pages
+git push -f git@github.com:telepenin/kernelcare-doc.git master:gh-pages
 
 cd -
