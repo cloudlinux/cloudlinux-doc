39e4ce052c81bd8270aca048ad41265a24f9bf42
Anuar Tastembekov <tastembekov.anuar@gmail.com>
2019-05-07 13:50:02 +0600

    HELP-17 Improve redirects

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -4,11 +4,8 @@ export default ({
   router, // the router instance for the app
   siteData // site metadata
 }) => {
-  const redirectionMapping = siteData.themeConfig.redirectionMapping;
   router.beforeEach((to, from, next) => {
-    if (redirectionMapping && redirectionMapping[to.redirectedFrom]) {
-      next(redirectionMapping[to.redirectedFrom]);
-    } else if (to.fullPath === "/") {
+    if (to.fullPath === "/") {
       next(siteData.themeConfig.defaultURL);
     } else {
       next();
