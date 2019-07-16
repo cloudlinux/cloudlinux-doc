c0fa6a4840b0ffbd3e0d8006a8a2d39df61aec14
Nikolay Telepenin <ntelepenin@cloudlinux.com>
2019-07-16 10:17:38 +0300

    Updating prod+test from one deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,8 +9,16 @@ yarn docs:build
 # navigate into the build output directory
 cd docs/.vuepress/dist
 
+if [[ "$CIRCLE_BRANCH" == 'dev' ]]; then
+    domain="docs.cloudlinux.com"
+    repo="cloudlinux-doc"
+else
+    domain="docs-dev.cloudlinux.com"
+    repo="cloudlinux-doc-test"
+fi
+
 # if you are deploying to a custom domain
-echo 'docs.cloudlinux.com' > CNAME
+echo $domain > CNAME
 
 git config --global user.email "telepenin.nikolay@gmail.com"
 git config --global user.name "circle-ci"
@@ -20,6 +28,6 @@ git add -A
 git commit -m 'deploy [skip ci]'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:cloudlinux/cloudlinux-doc.git master:gh-pages
+git push -f git@github.com:cloudlinux/$repo.git master:gh-pages
 
 cd -
