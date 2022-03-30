340d8254c28b9ab1830aac1bdf7bdf0dc4360970
jdaveyancloudlinux <86103906+jdaveyancloudlinux@users.noreply.github.com>
2022-03-30 19:23:20 +0400

    adding gtm

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -85,6 +85,15 @@ s.async = true;
 var e = document.getElementsByTagName('script')[0];
 e.parentNode.insertBefore(s, e);
 })(window);`
+    ],
+    [
+      "script",
+      {},
+      `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-T538N4K');`
     ]
   ],
   locales: {
